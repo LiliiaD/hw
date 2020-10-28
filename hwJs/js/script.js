aba1e357e7d0476a4789eeafae7c5f705df95ccc
unknown <lili4kadrebot@gmail.com>
2020-10-28 17:25:23 +0200

    HW JS1

---
+++ hwJs/js/script.js
@@ -0,0 +1,131 @@
+//Task 1
+//let greeting = 'hello';
+//let companyName = 'owu';
+//let lang = 'ua';
+//let number1 = 1;
+//let number2 = 10;
+//let number3 = -999;
+//let number4 = 123;
+//let numberPi = 3.14;
+//let fraction = 2.7;
+//let number5 = 16;
+//let is = true;
+//let has = false;
+
+//console.log(greeting, companyName, lang, number1, number2, number3, number4, numberPi, fraction, number5, is, has);
+//alert(greeting);
+//alert(companyName);
+//alert(lang);
+//alert(number1);
+//alert(number2);
+//alert(number3); 
+//alert(number4);
+//alert(numberPi);
+//alert(fraction);
+//alert(number5); 
+//alert(is);
+//alert(has);
+//document.write(`${greeting}, ${companyName}, ${lang}, ${number1}, ${number2}, ${number3}, ${number4}, ${numberPi}, ${fraction}, ${number5}, ${is}, ${has}`);
+
+//Task 2
+//greeting = 'hi';
+//companyName = 'okten';
+//lang = 'en';
+//number1 = 3;
+//number2 = 13;
+//number3 = -333;
+//number4 = 12;
+//numberPi = 3.1;
+//fraction = 2.4;
+//number5 = 8;
+//is =false;
+//has = true;
+//console.log(greeting, companyName, lang, number1, number2, number3, number4, numberPi, fraction, number5, is, has);
+//alert(greeting);
+//alert(companyName);
+//alert(lang);
+//alert(number1);
+//alert(number2);//
+//alert(number3); 
+//alert(number4);
+//alert(numberPi);
+//alert(fraction);
+//alert(number5); 
+//alert(is);
+//alert(has);
+////document.write(`${greeting}, ${companyName}, ${lang}, ${number1}, ${number2}, ${number3}, ${number4}, ${numberPi}, ${fraction}, ${number5}, ${is}, ${has}`);
+
+//Task 3
+//const day = 27;
+//const month = 10;
+//const year = 2020;
+
+//const dayName = 'Вівторок';
+//const monthName ='Жовтень';
+//const season = 'Осінь';
+
+//console.log(day, month, year, dayName, monthName, season);
+//alert(day);
+//alert(month);
+//alert(year);
+//alert(dayName);
+//alert(monthName);
+//alert(season);
+//document.write(`${day} ${month} ${year} ${dayName} ${monthName} ${season}`);
+
+//Task 4
+//const lastName = prompt('Enter your Last Name: '); 
+//const firstName = prompt('Enter your First Name: ');
+//const middleName = prompt('Enter your Middle Name: ');
+
+//console.log(lastName, firstName, middleName)
+//alert(lastName);
+//alert(firstName);
+//alert(middleName);
+//document.write(`${lastName} ${firstName} ${middleName}`);
+
+
+//Task 5//
+//const person = lastName + firstName +middleName;
+//console.log(person)
+
+//Task 15
+let a = 5 < 6;
+console.log(a);
+
+let b = 5 > 6;
+console.log(b);
+
+let c = 5 >= 6;
+console.log(c);
+
+let d = 5 == 6;
+console.log(d);
+
+let e = 10 == 10;
+console.log(e);
+
+let f = 10 <= 10;
+console.log(f);
+
+let g = 10 > 10;
+console.log(g);
+
+let h = 10 < 10;
+console.log(h);
+
+let i = 10 != 10;
+console.log(i);
+
+let j = 123 === '123';
+console.log(j);
+
+let k = 123 == '123';
+console.log(k);
+
+
+
+
+
+
+
